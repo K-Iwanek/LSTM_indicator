4abbaea445000a0992575a15bb410aef0919cdbd
K.Iwanek <Scarowak01@gmail.com>
2022-07-07 19:05:29 +0200

    work on Data_preparation.py

--- Data_preparation.py
+++ Data_preparation.py
@@ -15,22 +15,30 @@ import random
 
 ####Variables####
 
-SEQ_LEN = 1400    #How long of a preceeding sequence to collect for RNN 
-FASTEMA=10    #Technical indicator, EMA. We will look for FASTEMA and SLOWEMA crosses. 
-SLOWEMA=44    #Technical indicator, EMA. When FASTEMA line is greater than SLOWEMA line we are are looking for local max.
-LEARN_DATA=0.90 #Which part of data is learning data. The rest of the data will be used for testing
+SEQ_LEN = 1400    # How long of a preceeding sequence to collect for RNN 
+FASTEMA=10    # Technical indicator, EMA. We will look for FASTEMA and SLOWEMA crosses. 
+SLOWEMA=44    # Technical indicator, EMA. When FASTEMA line is greater than SLOWEMA line we are are looking for local max.
+LEARN_DATA=0.90 # Which part of data is learning data. The rest of the data will be used for testing
 
-####Loading the data####
-df=pd.read_csv('EURUSD_H1.csv', sep="\t")    #Downloaded from https://data.forexsb.com/data-app#
+#### Loading the data ####
+df=pd.read_csv('EURUSD_H1.csv', sep="\t")    # Downloaded from https://data.forexsb.com/data-app#
 
 
 ####FASTEMA and SLOWEMA calculation 
-df["FASTEMA"] = ta.ema(df["Close"], length=FASTEMA)
-df["SLOWEMA"] = ta.ema(df["Close"], length=SLOWEMA)
+df["Fastema"] = ta.ema(df["Close"], length=FASTEMA)
+df["Slowema"] = ta.ema(df["Close"], length=SLOWEMA)
 
 
-#### Dropping Nans
-df=df.dropna()
+#### Dropping Nans ####
+df.dropna()
 df.reset_index(drop=True, inplace=True)
 
 
+#### Searching for trend ####
+df['Trend'] = np.where(df['Fastema']>df['Slowema'], 1, 0)
+df['Trend-1']=df['Trend'].shift(periods=1)
+df["Trend_Change"]=df['Trend']-df['Trend-1']  # When Fastema starts to be smaller than Slowema: Trend_Change = -1. When greater: Trend_Change = 1
+
+#### Makeing list of interval's starts
+INTERVAL=df.index[df['Trend_Change'] != 0].tolist()  
+INTERVAL.pop(0) # 1st element is Na
